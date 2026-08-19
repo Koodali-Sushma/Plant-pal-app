7e99645369775453b407db7f06dadc2f9c0907aa
Sabrina <sabrinarach.sr@gmail.com>
2026-08-19 16:27:07 +0200

    delete  as it is later replaced by tailwind

--- components/navigation.js
+++ components/navigation.js
@@ -11,21 +11,21 @@ export default function Navigation() {
     <nav>
       <ul>
         <li>
-          <Link href="/" $isActive={router.pathname === "/"}>
+          <Link href="/">
             {/* link to my-plants */}
             <img src="/assets/plant.svg" alt="icon of a plant" />
             MyPlants
           </Link>
         </li>
         <li>
-          <Link href="/" $isActive={router.pathname === "/"}>
+          <Link href="/">
             {/* link to plant-list */}
             <img src="/assets/explore.svg" alt="icon of a compass" />
             Explore
           </Link>
         </li>
         <li>
-          <Link href="/" $isActive={router.pathname === "/"}>
+          <Link href="/">
             {/* link to rooms */}
             <img src="/assets/rooms.svg" alt="icon of a house" />
             Rooms
