eae375ed5e5085c9c6f63efc3f62a2049273418a
Sabrina <sabrinarach.sr@gmail.com>
2026-08-19 16:04:01 +0200

    write function and add links for three tabs

--- components/navigation.js
+++ components/navigation.js
@@ -1,12 +1,37 @@
 /* --- NAVIGATION BAR ---*/
 
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 export default function Navigation() {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        
-    )
+  /* !!!!! CORRECT LINKS MUST BE ADDED */
+  return (
+    <Nav>
+      <List role="list">
+        <li>
+          <Link href="/" $isActive={router.pathname === "/"}>
+            {" "}
+            {/* link to my-plants */}
+            MyPlants
+          </Link>
+        </li>
+        <li>
+          <Link href="/" $isActive={router.pathname === "/"}>
+            {" "}
+            {/* link to plant-list */}
+            Explore
+          </Link>
+        </li>
+        <li>
+          <Link href="/" $isActive={router.pathname === "/"}>
+            {" "}
+            {/* link to rooms */}
+            Rooms
+          </Link>
+        </li>
+      </List>
+    </Nav>
+  );
 }
