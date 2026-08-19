9fac311bb32382bd1eff3599009f01b0986f0e52
Sabrina <sabrinarach.sr@gmail.com>
2026-08-19 15:40:32 +0200

    create folder components and file for navigation

---
+++ components/navigation.js
@@ -0,0 +1,12 @@
+/* --- NAVIGATION BAR ---*/
+
+import {useRouter} from "next/router";
+import Link from "next/link";
+
+export default function Navigation() {
+    const router = useRouter();
+
+    return (
+        
+    )
+}
