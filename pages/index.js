7bd54ba70bdebc28e2ad1b9ead106fa7f7904dea
Sabrina <sabrinarach.sr@gmail.com>
2026-08-20 18:59:33 +0200

    delete plantList from homepage as it is on its own page

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,6 @@ export default function Homepage() {
           onCancel={() => setShowForm(false)}
         />
       )}
-      <PlantList onAddPlant={() => setShowForm(true)} />
     </>
   );
 }
