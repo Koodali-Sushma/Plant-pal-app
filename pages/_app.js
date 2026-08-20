321a1000d60e8c64176b18f2cbd5ba5056985abc
Jose Reyes <jose_reyes@outlook.de>
2026-08-20 14:45:47 +0200

    Add theme tokens and fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import "@/styles/globals.css";
 import Navigation from "@/components/navigation";
+import { Oswald } from "next/font/google";
+
+const heading = Oswald({
+  subsets: ["latin"],
+  weight: ["500"],
+  variable: "--font-heading",
+});
+
+const body = Lato({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  variable: "--font-body",
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
-      <Navigation />
+      <div className={`${heading.variable} ${body.variable} font-body`}>
+        <Component {...pageProps} />
+        <Navigation />
+      </div>
     </>
   );
 }
