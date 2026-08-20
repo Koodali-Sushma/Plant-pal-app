928224e68aa3cb517a5f38dedd3515d3695b3bbf
Sabrina <sabrinarach.sr@gmail.com>
2026-08-20 10:06:41 +0200

    change name to match database naming convention, add handleSubmit function

--- components/createPlantForm.js
+++ components/createPlantForm.js
@@ -2,11 +2,34 @@
 
 import Image from "next/image";
 
-export default function CreatePlantForm() {
+export default function CreatePlantForm({ onSubmitForm }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = {
+      name: formData.get("name"),
+      botanicalName: formData.get("botanicalName"),
+      imageUrl: formData.get("imageUrl"),
+      waterNeed: formData.get("waterNeed"),
+      lightNeed: formData.get("lightNeed"),
+      fertiliserSeason:
+        formData.getAll(
+          "fertiliserSeason",
+        ) /* getAll because the user can click multiple seasons */,
+      description: formData.get("description"),
+    };
+    onSubmitForm(data);
+    event.target.reset();
+  }
+
   return (
     <>
       <h2>Add a new plant</h2>
-      <form name="create-plant" aria-label="add a plant to your list">
+      <form
+        onSubmit={handleSubmit}
+        name="create-plant"
+        aria-label="add a plant to your list"
+      >
         {/* This is just a placeholder image, has to be replaced by a real import */}
         <label>Plant Image:</label>
         <Image
@@ -18,50 +41,50 @@ export default function CreatePlantForm() {
         <input type="text" id="name" name="name" required />
 
         <label htmlFor="botanical-name">Botanical Name:</label>
-        <input type="text" id="botanical-name" name="botanical-name" />
+        <input type="text" id="botanical-name" name="botanicalName" />
 
-        <label htmlFor="water-needs">Water Needs:</label>
+        <label htmlFor="water-need">Water Need:</label>
         <label>
-          <input type="radio" name="water-needs" value="low" required />
+          <input type="radio" name="waterNeed" value="Low" required />
           Low
         </label>
         <label>
-          <input type="radio" name="water-needs" value="medium" />
+          <input type="radio" name="waterNeed" value="Medium" />
           Medium
         </label>
         <label>
-          <input type="radio" name="water-needs" value="high" />
+          <input type="radio" name="waterNeed" value="High" />
           High
         </label>
 
-        <label htmlFor="light-needs">Light Needs:</label>
+        <label htmlFor="light-need">Light Need:</label>
         <label>
-          <input type="radio" name="light-needs" value="full-sun" required />
+          <input type="radio" name="lightNeed" value="Full Sun" required />
           Full Sun
         </label>
         <label>
-          <input type="radio" name="light-needs" value="partial-shade" />
+          <input type="radio" name="lightNeed" value="Partial Shade" />
           Partial Shade
         </label>
         <label>
-          <input type="radio" name="light-needs" value="full-shade" />
+          <input type="radio" name="lightNeed" value="Full Shade" />
           Full Shade
         </label>
 
         <label>
-          <input type="checkbox" name="fertiliser-season" value="spring" />
+          <input type="checkbox" name="fertiliserSeason" value="Spring" />
           Spring
         </label>
         <label>
-          <input type="checkbox" name="fertiliser-season" value="summer" />
+          <input type="checkbox" name="fertiliserSeason" value="Summer" />
           Summer
         </label>
         <label>
-          <input type="checkbox" name="fertiliser-season" value="autumn" />
+          <input type="checkbox" name="fertiliserSeason" value="Autumn" />
           Autumn
         </label>
         <label>
-          <input type="checkbox" name="fertiliser-season" value="winter" />
+          <input type="checkbox" name="fertiliserSeason" value="Winter" />
           Winter
         </label>
 
