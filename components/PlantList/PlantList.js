cc830ed29d33f5f010953eb53d1c07165a211786
SaskiaWeiß1234 <saskia.weiss@bstableau.de>
2026-08-20 15:09:47 +0200

    added styling to the component

--- components/PlantList/PlantList.js
+++ components/PlantList/PlantList.js
@@ -17,23 +17,27 @@ export default function PlantList(plants) {
     }
 
 return (
-    <div className="grid grid-cols-2  gap-5 m-1.5 list-none w-200">
+    <div className="grid grid-cols-2 m-auto gap-5  list-none w-100">
     {data.map((plants) => (
-        <li key={plants._id} className="relative h-48 w-full overflow-hidden rounded-xl border">
-            <Link className="absolute left-0 top-0 flex flex-col p-3" href={`/${plants._id}`}><span className="font-semibold text-white drop-shadow-md">
+        <li key={plants._id} className=" group relative h-48 w-48 overflow-hidden rounded-xl border">
+            {plants.imageUrl && (  
+             <Image
+            placeholder="blur"
+            blurDataURL={plants.imageUrl}
+            src={plants.imageUrl}
+            alt={plants.name}
+            fill
+            sizes="(max-width: 768px) 50vw, 300px"
+            className="object-cover transition-transform duration-300  z-0"/> 
+             )}
+            <div className="pointer-events-none absolute inset-x-0 bottom-0 h-[30%] bg-linear-to-t from-black/70 to-transparent z-1" />            <Link className="absolute left-0 top-0 flex flex-col p-3" href={`/${plants._id}`}>
+            <span className="font-semibold text-white drop-shadow-md z-1 absolute-bottom-0 left-0">
             {plants.name} 
             </span> 
-            <span className="text-sm text-white/90 drop-shadow-md">
+            <span className="text-sm text-blue drop-shadow-md">
              {plants.botanicalName}</span>
              </Link>
-             {plants.imageUrl && (  
-             <Image
-            src={plants.imageUrl}
-            width={500}
-            height={500}
-            alt="Picture of the plant"
-            className="object-cover"/> 
-             )}
+            
         </li>
     ))}
     </div>
--- pages/PlantList.js
+++ pages/PlantList.js
@@ -6,16 +6,27 @@ import { useState } from "react";
 import Image from "next/image";
 
 export default function PlantListPage() {
-  const { data, isLoading } = useSWR("/api/plants");
-  console.log(`data: ${data}`);
+     const { data, isLoading } = useSWR("/api/plants");
+        console.log(`data: ${data}`);
+    
+        if(isLoading) {
+            return <h1>Loading...</h1>;
+        }
+    
+        if (!data) {
+            return null;
+        }
+  return (
+    <>
+    <Link
+        href="/"
+        className="mb-4 inline-block text-emerald-400 hover:underline"
+      >
+        ← Back to My Plants
+      </Link>
+    <h1 className="mb-8 text-center text-4xl font-bold tracking-tight text-emerald-400 sm:text-5xl sticky">All Plants</h1>
+    <PlantList plants={data} />
 
-  if (isLoading) {
-    return <h1>Loading...</h1>;
-  }
-
-  if (!data) {
-    return null;
-  }
-
-  return <PlantList plants={data} />;
+    </>
+  );
 }
