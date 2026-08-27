bd889c0cb148a4b39a79e7b3906b196b335d3fe0
Sushma Koodali <sushma.gargya@outlook.com>
2026-08-27 14:38:18 +0200

    Config changes

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,10 @@ const nextConfig = {
         hostname: "cdn.midjourney.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
     ],
   },
 };
