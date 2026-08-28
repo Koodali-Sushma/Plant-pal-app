7efc9e1696b1a59a995a4282fe871aaf9f3c577b
Jose Reyes <jose_reyes@outlook.de>
2026-08-28 11:33:39 +0200

    Fix plus button alignment and card styling consistency

--- pages/index.js
+++ pages/index.js
@@ -53,15 +53,17 @@ export default function Homepage() {
   return (
     <main className="px-4 py-6">
       {!showForm && (
-        <button type="button" onClick={() => setShowForm(true)}>
+        <button
+          type="button"
+          onClick={() => setShowForm(true)}
+          className="fixed bottom-25 right-5 z-50 flex h-14 w-14 items-center
+        justify-center rounded-full bg-accent-500 shadow-2xl transition hover:bg-(--color-primary-700) hover:shadow-xl"
+        >
           <Image
             src="/assets/plus.svg"
             alt="plus sign"
-            width={40}
-            height={40}
-            className="fixed bottom-20 right-5 z-50 flex h-14 w-14 
-            items-center justify-center rounded-full bg-accent-500 
-            shadow-2xl transition hover:bg-primary-700 hover:shadow-xl"
+            width={48}
+            height={48}
           />
         </button>
       )}
